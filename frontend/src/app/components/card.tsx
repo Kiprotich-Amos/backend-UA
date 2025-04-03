096917da31954c8908db35cf848d97dc35fa8bb3
Kiprotich-Amos <kiprotichamoh11@gmail.com>
2025-04-03 16:29:57 +0300

    work 0.4

--- frontend/src/app/components/card.tsx
+++ frontend/src/app/components/card.tsx
@@ -1,2 +1,29 @@
 import React from "react";
+import styles from '@/app/components/css/Card.module.css'
 
+interface CardProps{
+    title?: string;
+    children: React.ReactNode;
+    actions?: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> =({title, children, actions}) =>{
+    return(
+        <div className={styles.card}>
+            {title && (
+                <div className={styles.cardHeader}>
+                    <h3>{title}</h3>
+                </div>
+            )}
+            <div className={styles.cardBody}>
+                {children}
+            </div>
+            {actions &&(
+                <div className={styles.carFooter}>
+                    {actions}
+                </div>
+            )}
+        </div>
+    );
+};
+export default Card;
